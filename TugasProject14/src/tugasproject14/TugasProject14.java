5834d0e94405be44551a9c3de128a9b80e60ebc1
Dini Choirunnisa <dinichoirunnisaa@gmail.com>
2018-05-16 12:01:06 +0700

    belajar

--- TugasProject14/src/tugasproject14/TugasProject14.java
+++ TugasProject14/src/tugasproject14/TugasProject14.java
@@ -3,33 +3,60 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tugasproject13;
+package tugasproject14;
+
+import java.util.Scanner;
 
 /**
  *
  * @author GeeksFarm
  */
-public class TugasProject13 {
+public class TugasProject14 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    //PENGURANGAN MATRIKS DATA OTOMATIS
+    //PENGURANGAN MATRIKS DENGAN INPUT DATA SENDIRI
         
         int matriks_a[][]=new int[2][2];
     int matriks_b[][]=new int[2][2];
     
-    matriks_a[0][0]=8;
-    matriks_a[0][1]=7;
-    matriks_a[1][0]=6;
-    matriks_a[1][1]=5;
+    Scanner input_matriks=new Scanner(System.in);
     
-    matriks_b[0][0]=1;
-    matriks_b[0][1]=2;
-    matriks_b[1][0]=3;
-    matriks_b[1][1]=4;
+    System.out.println("INPUT MATRIKS A");
+    for (int i=0;i<2;i++)
+    {
+        for (int j=0;j<2;j++)
+        {
+            System.out.print ("masukkan angka untuk matriks A"+"["+i+"] ["+j+"] :");
+            matriks_a[i][j]=input_matriks.nextInt();
+        }   
+    }
+    System.out.println ();
     
+    System.out.println("INPUT MATRIKS B");
+    for (int i=0;i<2;i++)
+    {
+        for (int j=0;j<2;j++)
+        {
+            System.out.print ("masukkan angka untuk matriks B"+"["+i+"] ["+j+"] :");
+            matriks_b[i][j]=input_matriks.nextInt();
+        }
+    }
+    System.out.println ();
+    
+    /*
+    matriks_a[0][0]=1;
+    matriks_a[0][1]=2;
+    matriks_a[1][0]=3;
+    matriks_a[1][1]=4;
+    
+    matriks_b[0][0]=5;
+    matriks_b[0][1]=6;
+    matriks_b[1][0]=7;
+    matriks_b[1][1]=8;
+    */
     int hasil_pengurangan[][]=new int[2][2];
     
     
@@ -39,6 +66,13 @@ public class TugasProject13 {
             hasil_pengurangan[i][j]=matriks_a[i][j]-matriks_b[i][j];
         }
     
+    /*
+    hasil_penjumlahan[0][0]= matriks_a[0][0]+matriks_b[0][0];
+    hasil_penjumlahan[0][1]= matriks_a[0][1]+matriks_b[0][1];
+    hasil_penjumlahan[1][0]= matriks_a[1][0]+matriks_b[1][0];
+    hasil_penjumlahan[1][1]= matriks_a[1][1]+matriks_b[1][1];
+    */    
+
     System.out.println ("MATRIKS A");
     for (int i=0;i<2;i++)
     {
@@ -61,7 +95,9 @@ public class TugasProject13 {
         }
         System.out.println ("]");
     }
-        
+    
+    
+    
     System.out.println();
     System.out.println ("MATRIKS A + MATRIKS B");
     for (int i=0;i<2;i++)
@@ -73,6 +109,11 @@ public class TugasProject13 {
         }
         System.out.println ("]");
     }    
+        
+    /*
+    System.out.print ("[ "+hasil_penjumlahan[0][0]+"  "+hasil_penjumlahan[0][1]+" ]");
+    System.out.print ("[ "+hasil_penjumlahan[1][0]+"  "+hasil_penjumlahan[1][1]+" ]");
+    */
     }
     
 }
